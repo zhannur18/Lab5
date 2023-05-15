ffbe33e062c00a1745639f9e0d1be09493b09cf3
Zhannur <221952@astanait.edu.kz>
2023-05-15 09:42:42 +0600

    class BST

---
+++ src/BST.java
@@ -0,0 +1,13 @@
+public class BST <K extends Comparable<K>, V>{
+    private Node root;
+    private class Node {
+        private K key;
+        private V val;
+        private Node left, right;
+        public Node(K key, V val){
+            this.key = key;
+            this.val = val;
+        }
+    }
+    public void put(K key, V val) {}
+}
