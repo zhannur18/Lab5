e8d08539f34aec7db58c9d2263da95a735dd709f
Zhannur <221952@astanait.edu.kz>
2023-05-21 16:48:31 +0600

    method put

--- src/BST.java
+++ src/BST.java
@@ -9,7 +9,25 @@ public class BST <K extends Comparable<K>, V>{
             this.val = val;
         }
     }
-    public void put(K key, V val) {
-
+    public void put(K key, V val) { // Method to insert a key-value pair into the BST
+        root = put(root, key, val);
     }
+    // Recursive helper method to insert a key-value pair
+    private Node put(Node node, K key, V val) {
+        if (node == null) {
+            size++;
+            return new Node(key, val);
+        }
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0)
+            node.left = put(node.left, key, val); // Traverse to the left subtree if key is less
+        else if (cmp > 0)
+            node.right = put(node.right, key, val); // Traverse to the right subtree if key is greater
+        else
+            node.val = val; // Update the value if key already exists
+
+        return node;
+    }
+
 }
